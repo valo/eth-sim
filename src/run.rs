2fbcc375a0f957b766827bb681745302b26a8500
Valentin Mihov <valentin.mihov@gmail.com>
2023-09-02 19:33:09 +0300

    Removed the global config so that the code is more modular

--- src/run.rs
+++ src/run.rs
@@ -1,14 +1,15 @@
-use ethers_providers::Middleware;
+use ethers_providers::{Middleware, Provider, Http};
 use ethers_core::types::H256;
 use eyre::{Result, WrapErr};
 use foundry_config::{find_project_root_path, Config, ethers_solc::EvmVersion};
 use foundry_evm::{
-    executor::{inspector::cheatcodes::util::configure_tx_env, opts::EvmOpts, RawCallResult},
+    executor::{inspector::cheatcodes::util::configure_tx_env, opts::EvmOpts, RawCallResult, fork::CreateFork},
     revm::primitives::U256 as rU256,
     trace::TracingExecutor,
     utils::h256_to_b256,
 };
-use foundry_cli::{utils, opts::RpcOpts};
+use foundry_cli::{opts::RpcOpts};
+use revm::primitives::Env;
 
 /// CLI arguments for `cast run`.
 #[derive(Debug, Clone)]
@@ -30,6 +31,21 @@ pub struct RunArgs {
     pub evm_version: Option<EvmVersion>,
 }
 
+pub async fn get_fork_material(
+    fork_url: String,
+    fork_block_number: Option<u64>,
+    mut evm_opts: EvmOpts,
+) -> eyre::Result<(Env, Option<CreateFork>)> {
+    evm_opts.fork_url = Some(fork_url.clone());
+    evm_opts.fork_block_number = fork_block_number;
+
+    let env = evm_opts.evm_env().await?;
+
+    let fork = Some(CreateFork { url: evm_opts.fork_url.clone().unwrap(), enable_caching: true, env: env.clone(), evm_opts: evm_opts.clone() });
+
+    Ok((env, fork))
+}
+
 impl RunArgs {
     /// Executes the transaction by replaying it
     ///
@@ -38,12 +54,10 @@ impl RunArgs {
     /// Note: This executes the transaction(s) as is: Cheatcodes are disabled
     pub async fn run(self) -> Result<RawCallResult> {
         let figment =
-            Config::figment_with_root(find_project_root_path(None).unwrap()).merge(self.rpc);
+            Config::figment_with_root(find_project_root_path(None).unwrap());
         let evm_opts = figment.extract::<EvmOpts>()?;
-        let mut config = Config::from_provider(figment).sanitized();
 
-        let provider = utils::get_provider_builder(&config)?
-            .build()?;
+        let provider = Provider::<Http>::try_from(self.rpc.url.as_ref().unwrap())?;
 
         let tx_hash: H256 = self.tx_hash.parse().wrap_err("invalid tx hash")?;
         let tx = provider
@@ -56,9 +70,13 @@ impl RunArgs {
             .ok_or_else(|| eyre::eyre!("tx may still be pending: {:?}", tx_hash))?
             .as_u64();
 
-        config.fork_block_number = Some(tx_block_number - 1);
+        let fork_block_number = Some(tx_block_number - 1);
 
-        let (mut env, fork, _chain) = TracingExecutor::get_fork_material(&config, evm_opts).await?;
+        let (mut env, fork) = get_fork_material(
+            self.rpc.url.unwrap(),
+            fork_block_number,
+            evm_opts
+        ).await?;
 
         let mut executor =
             TracingExecutor::new(env.clone(), fork, self.evm_version, self.debug).await;
